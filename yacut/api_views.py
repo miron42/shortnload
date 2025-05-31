8890a7eff190ec28849506a5fa5f624198bfc99b
prettyboy <prettyboymiron@yandex.ru>
2025-05-31 18:26:33 +0300

    1

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -9,11 +9,9 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 
 @bp.route('/id/', methods=['POST'])
 def create_short_link():
-    if not request.is_json:
-        return jsonify(message='Отсутствует тело запроса'), 400
-
     try:
-        data = request.get_json()
+        data = request.get_json(force=True)
+
     except Exception:
         return jsonify(message='Отсутствует тело запроса'), 400
 
@@ -26,35 +24,20 @@ def create_short_link():
     if not url:
         return jsonify(message='"url" является обязательным полем!'), 400
 
-    # если без custom_id и URL уже есть
     existing_by_url = URLMap.query.filter_by(original=url).first()
     if not custom_id and existing_by_url:
-        return (
-            jsonify(
-                short_link=url_for('main.redirect_view',
-                                   short=existing_by_url.short,
-                                   _external=True),
-                message='Короткая ссылка для переданного URL уже существует.'
-            ),
-            409
-        )
+        return jsonify(
+            short_link=url_for('main.redirect_view',
+                               short=existing_by_url.short, _external=True),
+            message='Короткая ссылка для переданного URL уже существует.'
+        ), 409
 
     if custom_id:
-        # длиннее 16 → та же ошибка, что и для недопустимого имени
-        if len(custom_id) > 16:
-            return jsonify(
-                message='Указано недопустимое имя для короткой ссылки'), 400
+        if len(custom_id) > 16 or not re.fullmatch(r'[a-zA-Z0-9]+', custom_id):
+            return jsonify(message='Указано недопустимое имя для короткой ссылки'), 400
 
-        if not re.fullmatch(r'[a-zA-Z0-9]+', custom_id):
-            return jsonify(
-                message='Указано недопустимое имя для короткой ссылки'), 400
-
-        existing_by_short = URLMap.query.filter_by(short=custom_id).first()
-        if custom_id.lower() == 'files' or existing_by_short:
-            return jsonify(message=(
-                'Предложенный вариант короткой ссылки '
-                'уже существует.'
-            )), 400
+        if custom_id.lower() == 'files' or URLMap.query.filter_by(short=custom_id).first():
+            return jsonify(message='Предложенный вариант короткой ссылки уже существует.'), 400
 
         short = custom_id
     else:
