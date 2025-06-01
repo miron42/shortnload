86c4bf580b6aeaf5b55851a26bb0132e8880fd2e
prettyboy <prettyboymiron@yandex.ru>
2025-06-01 18:24:06 +0300

    3

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,3 +1,4 @@
+from . import views, api_views, error_handlers
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
@@ -10,7 +11,8 @@ load_dotenv()
 
 # Создание и конфигурация приложения
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'DATABASE_URI', 'sqlite:///db.sqlite3')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'S3CR3T-K3Y-F0R-Y4CUT')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['WTF_CSRF_ENABLED'] = False
@@ -21,4 +23,3 @@ csrf = CSRFProtect(app)
 migrate = Migrate(app, db)
 
 # Импорт и регистрация представлений и обработчиков ошибок
-from . import views, api_views, error_handlers
